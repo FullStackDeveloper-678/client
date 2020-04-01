454d5f9b45fa42e17b612e01fbb8193a2765c234
Brower Wesley <browerwesley@Browers-Mac-mini.local>
2020-04-02 02:02:19 +0800

    update

---
+++ build/precache-manifest.599b36a5cf4158d77dfa86d4a3492818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37a96750388a744225b8c905ec716f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4b804a63dce1ef61b5",
+    "url": "/static/css/2.7c2b4f5f.chunk.css"
+  },
+  {
+    "revision": "3dc374351ba46a35d9b6",
+    "url": "/static/css/main.36a65f90.chunk.css"
+  },
+  {
+    "revision": "4e4b804a63dce1ef61b5",
+    "url": "/static/js/2.301459ab.chunk.js"
+  },
+  {
+    "revision": "edeb08c4b3836437e889089a9bab9d54",
+    "url": "/static/js/2.301459ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc374351ba46a35d9b6",
+    "url": "/static/js/main.6a28b4da.chunk.js"
+  },
+  {
+    "revision": "6880d9faefa5d5b46c1b",
+    "url": "/static/js/runtime-main.e305ba98.js"
+  }
+]);
